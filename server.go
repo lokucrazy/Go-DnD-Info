1cad757af99a2e9217c8d3f32e779aad1a3a5bc1
Ryan Lokugamage <Ryan@mwc-088244.dhcp.missouri.edu>
2019-03-13 18:05:16 -0500

    added content-type to header

--- server.go
+++ server.go
@@ -14,11 +14,13 @@ func main() {
 	r.Route("/get", func(r chi.Router) {
 
 		r.Get("/{table}", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
 			response, err := get(chi.URLParam(r, "table"), "")
 			writeResponse(w, response, err)
 		})
 
 		r.Get("/{table}/{name}", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
 			name := strings.Replace(chi.URLParam(r, "name"), "_", " ", -1)
 			response, err := get(chi.URLParam(r, "table"), name)
 			writeResponse(w, response, err)
